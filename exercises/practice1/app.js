0ddee6ae228b7383c7e778f87bad3b998b21f7a0
Tom Eschler <tomeschlercoding@gmail.com>
2019-04-17 17:27:20 -0600

    added daily-planet to projects

---
+++ exercises/practice1/app.js
@@ -0,0 +1,13 @@
+//Camelcase
+var myVariableIsNamedVar='this is my variable'
+
+//Whitespace
+
+var myVar='stuff'
+if(myVar==='things') {
+    console.log ('cool')
+} else {
+    console.log('dumb')
+}
+
+//command / comments out selection
